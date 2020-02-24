9c50eb6902e5f95f12b3dae7fe2ec4503f4ff6ec
hidehidehorie <b1a1s2k3e5t8hupro@gmail.com>
2020-02-24 12:08:10 +0900

    rendering components for routes and switching between pages

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -5,6 +5,7 @@ import { Route } from 'react-router-dom';
 import './Blog.css';
 import './Posts/Posts';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
@@ -19,8 +20,10 @@ class Blog extends Component {
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact render={() => <h1>HOME</h1>} />
-                <Route path="/" render={() => <h1>HOME2</h1>} />
+                {/* <Route path="/" exact render={() => <h1>HOME</h1>} />
+                <Route path="/" render={() => <h1>HOME2</h1>} /> */}
+                <Route path="/" exact component={Posts} />
+                <Route path="/new-post" component={NewPost} />
             </div>
         );
     }
